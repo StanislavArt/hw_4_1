05fe9e11eb8cc5f4db54b4559891b90de7bb9a6c
Stanislav <artemiev.stanislav@gmail.com>
2022-11-13 01:44:40 +0700

    homework 4.1

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -1,9 +1,13 @@
 package ru.hogwarts.school.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.AvatarRecord;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -12,4 +16,6 @@ public interface AvatarRepository extends JpaRepository<Avatar, Long> {
     Optional<Avatar> findById(Long id);
 
     Optional<Avatar> findByStudentId(Long id);
+	
+	Page<AvatarRecord> findAllBy(PageRequest pageRequest);
 }
